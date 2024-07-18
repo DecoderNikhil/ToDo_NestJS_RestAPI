409675990c81c269535fab90f8b5e96d868bd908
Nikhil <nikhils9802@gmail.com>
2024-07-18 14:30:57 +0530

    initial commit

---
+++ src/auth/dto/login.dto.ts
@@ -0,0 +1,12 @@
+import { IsString, Length, IsEmail, IsNotEmpty } from 'class-validator';
+
+export class LoginDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(6, 20)
+  password: string;
+}
